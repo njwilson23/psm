1ca226ff258fedb33cf0869e33866f381be52652
Nat Wilson <njwilson23@gmail.com>
2014-10-25 23:38:19 -0400

    Overly complex code for mapping parameter spaces by brute force

---
+++ parametermap.py
@@ -0,0 +1,73 @@
+
+import collections
+import itertools
+from functools import reduce
+import operator
+
+class ParameterMap(collections.abc.MutableMapping):
+    """ Tree structure for storing parameter realizations and model solutions.
+    """
+    def __init__(self, names, values):
+        self.names = names
+        self.values = values
+        self.tree = _buildtree(self.values)
+        return
+
+    def __getitem__(self, addr):
+        return _getleaf(self.tree, addr)
+
+    def __setitem__(self, addr, solution):
+        _setleaf(self.tree, addr, solution)
+        return
+
+    def __delitem__(self, addr):
+        _delleaf(self.tree, addr)
+
+    def __iter__(self):
+        return itertools.product(*self.values)
+
+    def __len__(self):
+        return reduce(operator.mul, (len(v) for v in self.values))
+
+    def fix_parameters(self, fixparameters):
+        fixnames = [p.name for p in fixparameters]
+        fixvalues = [p.value for p in fixparameters]
+
+        newnames = [name for name in self.names if name not in fixnames]
+        newvalues = [v for n,v in zip(self.names, self.values) if n in newnames]
+        newmap = ParameterMap(newnames, newvalues)
+        newmapkey = {name: i for i, name in enumerate(newnames)}
+
+        for newaddr in newmap:
+            # construct the old address
+            oldaddr = tuple(newaddr[newmapkey[name]] if name in newnames \
+                                else fixvalues[fixnames.index(name)] \
+                                for name in self.names)
+            print(oldaddr)
+            newmap[newaddr] = self[oldaddr]
+        return newmap
+
+def _buildtree(values):
+    if len(values) == 0:
+        return None
+    else:
+        return {val: _buildtree(values[1:]) for val in values[0]}
+
+def _getleaf(tree, addr):
+    if len(addr) == 1:
+        return tree[addr[0]]
+    else:
+        return _getleaf(tree[addr[0]], addr[1:])
+
+def _setleaf(tree, addr, val):
+    if len(addr) == 1:
+        tree[addr[0]] = val
+    else:
+        _setleaf(tree[addr[0]], addr[1:], val)
+
+def _delleaf(tree, addr):
+    if len(addr) == 1:
+        del tree[addr[0]]
+    else:
+        _delleaf(tree[addr[0]], addr[1:])
+
---
+++ parameterspace.py
@@ -0,0 +1,127 @@
+""" Pure-Python tool for naively mapping multidimensional parameter spaces """
+
+import copy
+import collections.abc
+from math import log, exp, floor
+from parametermap import ParameterMap
+
+class Parameter(object):
+
+    def __init__(self, name, bounds, min_step=None):
+        self.scale = "linear"
+        self.name = name
+        self.bounds = bounds
+        self.values = []
+        if min_step is None:
+            self.min_step = (bounds[1] - bounds[0])/100.0
+        else:
+            self.min_step = min_step
+        return
+
+    def __repr__(self):
+        return "<Parameter[{0}]({1}:{2})>".format(self.name,
+                                                  self.bounds[0],
+                                                  self.bounds[1])
+
+    def partition(self, n):
+        if n < 2:
+            raise ValueError("Partitions must be greater than 1")
+        elif n > self.npossible():
+            raise ValueError("Partitions must be fewer than {0}".format(self.npossible()))
+
+        if self.scale == "linear":
+            dx = (self.bounds[1] - self.bounds[0]) / (n-1)
+            self.values = [self.bounds[0]+dx*i for i in range(n)]
+        elif self.scale == "log":
+            dx = (log(self.bounds[1]) - log(self.bounds[0])) / (n-1)
+            self.values = [exp(log(self.bounds[0])+dx*i) for i in range(n)]
+        return self.values
+
+    def npossible(self):
+        return int(floor((self.bounds[1] - self.bounds[0]) / self.min_step))+1
+
+    def fixed_at_index(self, i):
+        return FixedParameter(self.name, self.values[i])
+
+class FixedParameter(object):
+
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+    def __repr__(self):
+        return "<FixedParameter[{0}]({1})>".format(self.name, self.value)
+
+class ParameterSpace(collections.abc.Container):
+
+    def __init__(self, parameters, model_call, default_dict=None):
+
+        """ ParameterSpace manages a suite of model runs.
+        
+        Arguments:
+        ----------
+        `parameters::list` should be a set of parameters to vary.
+
+        `model_call::function` should be a callable that accepts a dictionary
+        of parameters, performs a model run, and return a solution or a
+        reference to a solution.
+        """
+        assert(hasattr(parameters, "__iter__"))
+        assert(hasattr(model_call, "__call__"))
+        self.parameters = parameters
+        self.model_call = model_call
+        if default_dict is None:
+            self.default_dict = {p.name:None for p in parameters}
+        else:
+            self.default_dict = default_dict
+        # self.realizations = {}
+        return
+
+    def __contains__(self, name):
+        return any(p.name == name for p in self.parameters)
+
+    def mapspace(self, N):
+        """ `N::list,dict` specifies the number of realizations to add along
+        each parameter. """
+        if hasattr(N, "keys"):
+            runs = {}
+            for name, n in N.items():
+                for p in self.parameters:
+                    if p.name == name:
+                        p.partition(n)
+                        runs[p] = p.values
+                        break
+        elif hasattr(N, "__iter__"):
+            if len(N) == len(self.parameters):
+                runs = {p:p.partition(n) for p,n in zip(self.parameters, N)}
+            else:
+                raise Exception("Have {0} parameters but recieved {1} "
+                                "values".format(len(self.parameters), len(N)))
+        elif isinstance(N, int):
+            if N > sum(p.npossible() for p in self.parameters):
+                raise ValueError("N is larger than the total number of "
+                                 "realizations allowed")
+            npars = len(self.parameters)
+            nperparam = dict([(p, 0) for p in self.parameters])
+            i = n = 0
+            while n != N:
+                p = self.parameters[i%npars]
+                if nperparam[p] < p.npossible():
+                    nperparam[p] += 1
+                    n += 1
+                i += 1
+
+            names = [p.name for p in self.parameters]
+            values = [p.partition(nperparam[p]) for p in self.parameters]
+
+        pmap = ParameterMap(names, values)
+
+        for combo in pmap:
+            parameter_dict = copy.copy(self.default_dict)
+            for p, val in zip(self.parameters, combo):
+                parameter_dict[p.name] = val
+            res = self.model_call(parameter_dict)
+            pmap[combo] = res
+
+        return pmap
+
