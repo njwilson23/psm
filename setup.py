ae0bd75fb8e1dcc4abd050c0901a6a464a6af11a
Nat Wilson <njwilson23@gmail.com>
2014-11-01 12:54:50 -0400

    add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+
+setup(name='psm',
+      packages=find_packages(exclude="test*"),
+      author="Nat Wilson",
+      version='0.1a',
+      description='Naive parameter space sampler',
+      license='Contact me',
+      )
+
