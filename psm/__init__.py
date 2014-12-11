340b61631357bfb296eb1d31000986d46d3c22da
Nat Wilson <njwilson23@gmail.com>
2014-12-11 14:31:16 -0500

    move parameter types into their own submodule

--- psm/__init__.py
+++ psm/__init__.py
@@ -1,12 +1,12 @@
 
-from .parameterspace import ParameterSpace, \
-                            Parameter, \
-                            DiscreteValueParameter, \
-                            FixedParameter
-
 from .core import fillspace, latin_hypercube
-
 from .parametermap import ParameterMap
+from .parameters import Parameter, \
+                        DiscreteValueParameter, \
+                        FixedParameter
+
+# Deprecated
+from .parameterspace import ParameterSpace
 
 __all__ = ["parameterspace", "parametermap", "core"]
 
--- psm/parameterspace.py
+++ psm/parameterspace.py
@@ -2,79 +2,8 @@
 
 import copy
 import collections.abc
-from math import log, exp, floor
-from random import shuffle, random
+from random import shuffle
 from .parametermap import ParameterMap
-
-class Parameter(object):
-
-    def __init__(self, name, bounds, scale="linear"):
-        """ Defines a named parameter that can be automatically varied over a
-        range within *bounds*.
-
-        Keyword arguments:
-        `scale::string` is either "linear" or "log"
-        """
-        self.scale = scale
-        self.distribution = "uniform"
-        self.name = name
-        self.bounds = bounds
-        self.values = []
-        return
-
-    def __repr__(self):
-        return "<Parameter[{0}]({1}:{2})>".format(self.name,
-                                                  self.bounds[0],
-                                                  self.bounds[1])
-
-    def partition(self, n):
-        if n < 2:
-            raise ValueError("Partitions must be greater than 1")
-
-        if self.scale == "linear":
-            dx = (self.bounds[1] - self.bounds[0]) / (n-1)
-            self.values = [self.bounds[0]+dx*i for i in range(n)]
-        elif self.scale == "log":
-            dx = (log(self.bounds[1]) - log(self.bounds[0])) / (n-1)
-            self.values = [exp(log(self.bounds[0])+dx*i) for i in range(n)]
-        return self.values
-
-    def fixed_at_index(self, i):
-        return FixedParameter(self.name, self.values[i])
-
-class DiscreteValueParameter(Parameter):
-
-    def __init__(self, name, values):
-        """ Defines a named parameter that can be automatically varied over
-        discrete values.
-        """
-        self.distribution = "discrete"
-        self.name = name
-        self.possiblevalues = values
-        return
-
-    def __repr__(self):
-        return "<DiscreteValueParameter[{0}]{{1}}>".format(self.name,
-                                                           len(self.possiblevalues))
-
-    def partition(self, n):
-        N = len(self.possiblevalues)
-        if n < 2:
-            raise ValueError("Partitions must be greater than 1")
-        elif n > N:
-            raise ValueError("Only {0} partitions possible".format(N))
-
-        self.values = [self.possiblevalues[i] for i in range(0, N, N//n)]
-        return self.values
-
-class FixedParameter(object):
-
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
-
-    def __repr__(self):
-        return "<FixedParameter[{0}]({1})>".format(self.name, self.value)
 
 class ParameterSpace(collections.abc.Container):
 
